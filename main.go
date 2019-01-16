a795a6cbbcbbf65129006ecdc7266c1a7d80d851
veryspry <ehlinger.matt@gmail.com>
2019-01-16 14:55:53 -0600

    adds initial hackernews api integration

--- main.go
+++ main.go
@@ -1,66 +1,110 @@
 package main
 
 import (
-    "net/http"
-    "strings"
-    "fmt"
-    "./add"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
 )
 
-var testString string = "yeah, buddy"
+var testString = "yeah, buddy"
 
-func sayHello( w http.ResponseWriter, r *http.Request) {
-    message := r.URL.Path
-    fmt.Println("http.Request is here:", r.URL)
-    message = strings.TrimPrefix(message, "/")
-    message = "Hello " + message
-
-    w.Write([]byte(message))
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	message := r.URL.Path
+	fmt.Println("http.Request is here:", r.URL)
+	message = strings.TrimPrefix(message, "/")
+	message = "Hello " + message
+	w.Write([]byte(message))
 }
 
 func formatNums(w http.ResponseWriter, r *http.Request) {
-    nums := r.URL.Path
-    nums = strings.TrimPrefix(nums, "/addnums/")
-    w.Write([]byte(nums))
+	nums := r.URL.Path
+	nums = strings.TrimPrefix(nums, "/addnums/")
+	w.Write([]byte(nums))
 }
 
 func printShit() {
-    var s string = "hello"
-    fmt.Println(s)
-    s = "goodbye"
-    fmt.Println(s)
-    var p *string = &s
-    *p = "yaassss"
-    fmt.Println(s)
-    s = "whoa"
-    fmt.Println(s)
+	var s = "hello"
+	fmt.Println(s)
+	s = "goodbye"
+	fmt.Println(s)
+	var p = &s
+	*p = "yaassss"
+	fmt.Println(s)
+	s = "whoa"
+	fmt.Println(s)
 }
 
-func arrayLooper() {
-    a := [5]int{1, 2, 3, 4, 5}
-    fmt.Println("len:", len(a))
-    for i := 0; i<len(a); i++ {
-        fmt.Println(a[i])
-    }
+// Get and format http response body
+func fmtRes(url string) string {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	return string(body)
 }
 
-func stringLooper(str string) {
-    var len int = len(str)
-    for i := 0; i<len; i++ {
-        fmt.Println(string(str[i]))
-    }
-    return
+// Get all of the new news items
+func getNewsItems() []string {
+	url := "https://hacker-news.firebaseio.com/v0/newstories.json"
+	body := fmtRes(url)
+	storyIds := strings.TrimSuffix(strings.TrimPrefix(strings.TrimSpace(string(body)), "["), "]")
+	idSlice := strings.Split(storyIds, ",")
+	return idSlice
+}
+
+// Filter anything that doesn't have type field == "story"
+func filterStories(ids []string) []map[string]interface{} {
+	var s []map[string]interface{}
+
+	for i := 0; i < len(ids); i++ {
+		url := "https://hacker-news.firebaseio.com/v0/item/" + ids[i] + ".json?print=pretty"
+		body := fmtRes(url)
+		byt := []byte(body)
+		var dat map[string]interface{}
+		if err := json.Unmarshal(byt, &dat); err != nil {
+			panic(err)
+		}
+		if dat["type"] == "story" {
+			s = append(s, dat)
+		}
+	}
+	// fmt.Println(dat["type"])
+	return s
+}
+
+// Get all stories
+func getNews(w http.ResponseWriter, r *http.Request) {
+
+	ids := getNewsItems()
+	stories := filterStories(ids)
+	// fmt.Println("item", item)
+
+	// newest := getNewsItems()[0]
+	// // https://hacker-news.firebaseio.com/v0/item/192327.json?print=pretty
+	// url := "https://hacker-news.firebaseio.com/v0/item/" + newest + ".json?print=pretty"
+	// body := fmtRes(url)
+
+	fmt.Println("stories", stories[0])
+
+}
+
+func getStory(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("url", r.URL.Path)
 }
 
 func main() {
-    add.Adder(40, 54)
-    arrayLooper()
-    stringLooper(testString)
-    printShit()
-    http.Handle("/", http.FileServer(http.Dir("./public")))
-    http.HandleFunc("/writename", sayHello)
-    http.HandleFunc("/addnums/*", formatNums)
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        panic(err)
-    }
+	http.Handle("/", http.FileServer(http.Dir("./public")))
+	http.HandleFunc("/writename", sayHello)
+	http.HandleFunc("/addnums/*", formatNums)
+	http.HandleFunc("/news", getNews)
+	http.HandleFunc("/news/*", getStory)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
 }
